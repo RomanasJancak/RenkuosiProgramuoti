c00fe4aa03ea7d3eed165fd2f9eca30ce7caf915
MightyRomanas <romanas.jancak@gmail.com>
2022-10-18 15:04:28 +0300

    Stiliaus karkasas pradžia

--- Projektas_Biudzetas/index.php
+++ Projektas_Biudzetas/index.php
@@ -14,12 +14,37 @@ include "klases.php";
 </head>
 <body>
   <div class="main">
-    <div class="logo-placeholder"></div>
-    <div class="news-notifications"></div>
-    <div class="trumpa-bendra-suvestine"></div>
-    <div class="login-placeholder"></div>
-    <div class="nav-bar"></div>
-
+    <div class="logo-placeholder"><p>LOGOTIPAS</p></div>
+    <div class="news-notifications"><p>NAUJIENOS</p></div>
+    <div class="trumpa-bendra-suvestine"><p>Suvestine</p></div>
+    <div class="login-placeholder">
+      <div class="login-signin">
+        <form action="">
+          <label for="Login">Login :</label>
+          <input type="text" name="Login"></input>
+          <label for="password">Password</label>
+          <input type="password"></input>
+        </form>
+      </div>
+    </div>
+    <nav class="nav-bar">
+      <ul>
+        <li><a href="">Pagrindinis</a></li>
+        <li><a href="">Įplaukos</a></li>
+        <li><a href="">Išlaidos</a></li>
+        <li>Statistika</li>
+        <li>Pirkinių sąrašas</li>
+        <li>Apie</li>
+        <li>Kontaktai</li>
+      </ul>
+    </nav>
+    <div class="left-side"><p>left-side</p></div>
+    <div class="main-stuff">
+      <p>main-stuff</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, odio eveniet a quibusdam harum consequatur consequuntur omnis voluptates eos fuga obcaecati nam eaque, accusantium aspernatur maxime repudiandae nesciunt fugiat architecto?</p>
+    </div>
+    <div class="right-side"><p>right-side</p></div>
+    <div class="footer"><p>footer</p></div>
   </div> 
 </body>
 </html>
