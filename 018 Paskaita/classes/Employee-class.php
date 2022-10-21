2e8f34a012bf65b47c8653bdce878c8aec4817f9
MightyRomanas <romanas.jancak@gmail.com>
2022-10-21 16:42:29 +0300

    18 Paskaitos medziaga

---
+++ 018 Paskaita/classes/Employee-class.php
@@ -0,0 +1,23 @@
+<?php
+include "utilities/DatabaseManager.php"
+class Employee{
+
+    private $cols = array();
+
+    public function index(){
+        $databaseManager =  new DatabaseManager();
+    }
+    public function create(){
+
+    }
+    public function update(){
+
+    }
+    public function delete(){
+
+    }
+    public function get(){
+
+    }
+}
+?>
