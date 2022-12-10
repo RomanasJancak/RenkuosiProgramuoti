4370088882c4da321cdb8223f14f450a14c883ed
MightyRomanas <romanas.jancak@gmail.com>
2022-12-10 21:07:18 +0200

    1DocumentasPadarytas

---
+++ laravelProjektas4/laravel/database/migrations/2022_12_10_100351_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->String('title');
+            $table->String('isbn');
+            $table->unsignedBigInteger('pages');
+            $table->String('about');
+            $table->unsignedBigInteger('author_id');
+            $table->foreign('author_id')->references('id')->on('authors');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
