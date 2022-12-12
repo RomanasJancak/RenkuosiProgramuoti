8f1080afb78570255d3be8c5e3567aa3555e199f
MightyRomanas <romanas.jancak@gmail.com>
2022-12-12 18:08:40 +0200

    PradinisSukurimasLaravelProjekto

--- laravelProjektas4/laravel/resources/views/book/index.blade.php
+++ laravelProjektas4/laravel/resources/views/book/index.blade.php
@@ -1,3 +1,19 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+   <div class="row justify-content-center">
+       <div class="col-md-8">
+           <div class="card">
+               <div class="card-header">PAVADINIMAS</div>
+
+               <div class="card-body">
+                 BLADE TURINYS
+               </div>
+           </div>
+       </div>
+   </div>
+</div>
+@endsection
 @foreach ($books as $book)
   <a href="{{route('book.edit',[$book])}}">{{$book->title}} {{$book->bookAuthor->name}} {{$book->bookAuthor->surname}}</a>
   <form method="POST" action="{{route('book.destroy', [$book])}}">
@@ -6,3 +22,6 @@
   </form>
   <br>
 @endforeach
+
+
+
