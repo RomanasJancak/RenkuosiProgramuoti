c91a2df1af9ba8b77cd30965535c3abc3d922af6
Romanas Jančak <romanas.jancak@gmail.com>
2023-04-03 12:51:29 +0300

    Apvalymas

--- 003 Paskaita/get.php
+++
@@ -1,7 +0,0 @@
-<?php
-/*Rankinis užpildymas
-$puslapis = $_GET["puslapis"];
-echo "ddd   ".$puslapis;
-*/
-
-?>
--- laravelProjektas4/laravel/resources/views/views/book/index.blade.php
+++
@@ -1,8 +0,0 @@
-@foreach ($books as $book)
-  <a href="{{route('book.edit',[$book])}}">{{$book->title}} {{$book->bookAuthor->name}} {{$book->bookAuthor->surname}}</a>
-  <form method="POST" action="{{route('book.destroy', [$book])}}">
-   @csrf
-   <button type="submit">DELETE</button>
-  </form>
-  <br>
-@endforeach
