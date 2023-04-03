7f1cd5c00bd0d19eeeb9d8b987484f5d5ab0ecbf
Romanas Jančak <romanas.jancak@gmail.com>
2023-04-03 12:13:17 +0300

    20230403Rytas

--- Projektas_Biudzetas/SharedBiudzetasLaravel/resources/views/apsipirkimas/createToPrimaryBudget.blade.php
+++ Projektas_Biudzetas/SharedBiudzetasLaravel/resources/views/apsipirkimas/createToPrimaryBudget.blade.php
@@ -1,13 +1,20 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
-  <div class="row">
-    <div class="col-sm-1"> 
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur voluptates iusto natus doloribus libero ullam hic tenetur incidunt. Eum dicta architecto laboriosam perferendis illo nam est quisquam? Deserunt, hic autem.</p>     
+  <form action="">
+    <div class="form-group">
+      <label for="CountrySelect"></label>
+      <input type="text" class="form-control" id="CountrySelect" aria-describedby="CountryHelp" placeholder="Enter Country">
+      <small id="CountryHelp" class="form-text text-muted">Select Country where expense occured</small>
+    </div>    
+    <div class="form-group">
+      <label for="VendorSelect"></label>
+      <input type="text" class="form-control" id="VendorSelect" aria-describedby="VendorHelp" placeholder="Enter Vendor">
+      <small id="VendorHelp" class="form-text text-muted">Select vendor where expense occured</small>
     </div>
-    <div class="col-sm-2">
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste maiores rerum temporibus optio beatae ea obcaecati saepe odit soluta dolorum perspiciatis consectetur dolorem, repellat in consequuntur dicta doloribus at amet.</p>
+    <div class="form-group">
+
     </div>
-  </div>
+  </form>
 </div>     
 @endsection
